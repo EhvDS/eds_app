d29b587ee5fd4ede052cebefebde7fc86381ce6a
Simona Orzan <s.orzan@fontys.nl>
2023-03-17 08:25:03 +0100

    removed menu

--- app.py
+++ app.py
@@ -20,24 +20,20 @@ def aq_maxmin():
 
 # MAIN
 def main():
+    st.set_page_config(page_title='EDS' ,layout="wide",page_icon='')
     st.title("Eindhoven Data Stories")
-    menu = ["Home","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
-    if choice == "Home":
-        st.subheader("what the data says..")
-        col1,col2 = st.columns(2)
-        with col1:
-            maxmin = aq_maxmin()
-            st.write("The worst PM1 concentration measured today is {}".format(maxmin))
+    st.subheader("what the data says..")
+    col1,col2 = st.columns(2)
+    with col1:
+        maxmin = aq_maxmin()
+        st.write("The worst PM1 concentration measured today is {}".format(maxmin))
 
-        with col2:
-            with st.form(key="question"):
-                ta_question = st.text_area("Your data question here")
-                question = st.form_submit_button("Ask EDS")
+    with col2:
+        with st.form(key="question"):
+            ta_question = st.text_area("Your data question here")
+            question = st.form_submit_button("Ask EDS")
 
-    else:
-        st.subheader("About this site")
-        st.info("Made and maintained @ Fontys ICT")
+    st.info("Made and maintained @ Fontys ICT")
 
 if __name__ == '__main__':
     main()
