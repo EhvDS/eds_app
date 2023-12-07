56f3f7f20dc850bf016456d9f4551c664ef6c337
KevinGeertjens <K.Geertjens@gmail.com>
2023-12-07 22:57:23 +0100

    Implement Geoguessr gameplay loop
    Implemented a simple gameplay loop that let's players play 5 rounds, see their score, and reset to play again

--- pages/Geoguessr_Kevin_Geertjens.py
+++ pages/Geoguessr_Kevin_Geertjens.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import os
 from streamlit_pannellum import streamlit_pannellum
 
 # Loading images
@@ -18,6 +17,12 @@ if 'score' not in st.session_state: # number of correct guesses
 if 'image_index' not in st.session_state: # image_index controls the image that is shown
     st.session_state.image_index = 0
 
+if 'current_round' not in st.session_state: # image_index controls the image that is shown
+    st.session_state.current_round = 1
+
+n_rounds = 5
+game_done = st.session_state.current_round > n_rounds
+
 def cycle_image(): # TODO: make image selection random
     # Increase index or reset to 0 if max is reached
     if st.session_state.image_index < (len(image_keys)-1):
@@ -33,30 +38,51 @@ st.title("Geoguessr in Eindhoven")
 st.markdown("""Play Geoguessr with locations throughout Eindhoven: You will be shown several 360 degree pictures, and you have to guess which neighbourhood in Eindhoven the pictures 
                were taken in. You can pan and zoom the image to look around.""")
 
-st.subheader("Score: " + str(st.session_state.score) + " / " + str(len(image_keys)))
-st.subheader("Image: " + str(st.session_state.image_index + 1) + " / " + str(len(image_keys)))
-# Panorama viewer
-streamlit_pannellum(
-    config={
-      "default": {
-        "firstScene": "first",
-      },
-      "scenes": {
-        "first": {
-          "type": "equirectangular",
-          "panorama": selected_image,
-          "autoLoad": True,
-        }
-      }
-    }
-)
+col1, col2 = st.columns(2)
+col1.subheader("Score: " + str(st.session_state.score) + " / " + str(n_rounds))
+col2.subheader("Round: " + str(min(st.session_state.current_round, n_rounds)) + " / " + str(n_rounds))
+
+st.markdown("#")
 
 # Neighbourhood Selection
 # TODO: show map of Eindhoven instead of basic selectbox
 def check_answer(value):
     # Check answer given in selectbox, update score, and move to next image
     cycle_image()
+
+    st.session_state.current_round += 1
+    
     st.session_state.score += (value == correct_neighbourhood)
 
-selected_neighbourhood = st.selectbox('Which neighbourhood is this image located in?', (image_keys))
-st.button("Confirm Neighbourhood", on_click=check_answer, args=(selected_neighbourhood,))
+if not game_done:
+  selected_neighbourhood = st.selectbox('Which neighbourhood is this image located in?', (image_keys))
+  st.button("Confirm Neighbourhood", on_click=check_answer, args=(selected_neighbourhood,))
+
+# Panorama viewer
+if not game_done:
+  streamlit_pannellum(
+      config={
+        "default": {
+          "firstScene": "first",
+        },
+        "scenes": {
+          "first": {
+            "type": "equirectangular",
+            "panorama": selected_image,
+            "autoLoad": True,
+            "disabled": True
+          }
+        }
+      }
+  )
+
+# Game Over display
+def reset_game():
+    st.session_state.image_index = 0
+    st.session_state.score = 0
+    st.session_state.current_round = 1
+
+if game_done:
+    st.header("Game Over")
+    st.markdown("You guessed " + str(st.session_state.score) + " images correctly.")
+    st.button("New Game", on_click=reset_game)
