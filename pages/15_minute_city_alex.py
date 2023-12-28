b8f69339cc60d2b2b06275f3d54d6d1152b6ac5f
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-28 01:09:06 +0100

    load neighborhood data

--- pages/15_minute_city_alex.py
+++ pages/15_minute_city_alex.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Loading the data
-df_neighborhoods = pd.read_csv("./data/buurten_data_alex.csv")
+df_neighborhoods = pd.read_csv("./data/buurten_data_alex.csv", error_bad_lines=False)
 
 
 st.title("15 Minutes City")
