c6dd9c8caacdbc11ba9ca747ed7675742315af6e
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-28 01:16:17 +0100

    show the data

--- pages/15_minute_city_alex.py
+++ pages/15_minute_city_alex.py
@@ -4,7 +4,11 @@ import pandas as pd
 # Loading the data
 df_neighborhoods = pd.read_csv("./data/buurten_data_alex.csv", error_bad_lines=False)
 
+print(df_neighborhoods.head())
 
 st.title("15 Minutes City")
 
 st.subheader("Find the closest path from A to B")
+
+
+st.dataframe(df_neighborhoods)
