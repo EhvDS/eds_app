a4b1d6b09ce8beb5a9d0309e9ec663a77909527a
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-28 21:57:04 +0100

    Commented out code

--- pages/15_minute_city_alex.py
+++ pages/15_minute_city_alex.py
@@ -36,21 +36,21 @@ for _, row in df_neighborhoods.iterrows():
     ).add_to(m)
 
 # Add the connections between neighborhoods
-for i, row_i in df_neighborhoods.iterrows():
-    for j, row_j in df_neighborhoods.iterrows():
-        # The URL of the Google Maps Directions API
-        url = f"https://maps.googleapis.com/maps/api/directions/json?origin={row_i['latitude']},{row_i['longitude']}&destination={row_j['latitude']},{row_j['longitude']}&key={api_key}"
+# for i, row_i in df_neighborhoods.iterrows():
+#     for j, row_j in df_neighborhoods.iterrows():
+#         # The URL of the Google Maps Directions API
+#         url = f"https://maps.googleapis.com/maps/api/directions/json?origin={row_i['latitude']},{row_i['longitude']}&destination={row_j['latitude']},{row_j['longitude']}&key={api_key}"
 
-        # Send a request to the API
-        response = requests.get(url)
+#         # Send a request to the API
+#         response = requests.get(url)
 
-        # Parse the response JSON to get the route
-        route = response.json()['routes'][0]['legs'][0]['duration']['value']
+#         # Parse the response JSON to get the route
+#         route = response.json()['routes'][0]['legs'][0]['duration']['value']
 
-        # If the travel time is under the threshold
-        if route / 60 <= 15:
-            # Add a line between the neighborhoods on the map
-            folium.PolyLine([(row_i['latitude'], row_i['longitude']), (row_j['latitude'], row_j['longitude'])], color="red", weight=2.5, opacity=1).add_to(m)
+#         # If the travel time is under the threshold
+#         if route / 60 <= 15:
+#             # Add a line between the neighborhoods on the map
+#             folium.PolyLine([(row_i['latitude'], row_i['longitude']), (row_j['latitude'], row_j['longitude'])], color="red", weight=2.5, opacity=1).add_to(m)
 
 # Display the map in Streamlit
 folium_static(m)
