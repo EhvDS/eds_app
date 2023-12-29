3a86437c844e664d89f3c9733e00639dbc3b9aa3
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-29 11:45:54 +0100

    Updated map

--- pages/15_minute_city_alex.py
+++ pages/15_minute_city_alex.py
@@ -31,41 +31,40 @@ G = nx.Graph()
 def calculate_distance(point1, point2):
     return geodesic(point1, point2).km
 
-# Add nodes and edges to the graph
+
+# Create a map
+m = folium.Map(location=[df_neighborhoods['latitude'].mean(), df_neighborhoods['longitude'].mean()], zoom_start=13)
+
+# Add the GeoJSON data to the map and markers only for neighborhoods within the threshold
 for index, row in df_neighborhoods.iterrows():
     for index2, row2 in df_neighborhoods.iterrows():
         if index != index2:
             distance = calculate_distance((row['latitude'], row['longitude']), (row2['latitude'], row2['longitude']))
             if distance <= threshold:  # Only add connections under threshold
-                G.add_node(row['BUURTNAAM'], pos=(row['latitude'], row['longitude']))
-                G.add_node(row2['BUURTNAAM'], pos=(row2['latitude'], row2['longitude']))
-                G.add_edge(row['BUURTNAAM'], row2['BUURTNAAM'])
-
-# Draw the graph
-nx.draw(G, with_labels=True)
-st.pyplot()
-
-# Draw the graph
-nx.draw(G, with_labels=True)
-st.pyplot()
-
-
-# Create a map
-m = folium.Map(location=[df_neighborhoods['latitude'].mean(), df_neighborhoods['longitude'].mean()], zoom_start=13)
-
-# Add the GeoJSON data to the map
-for _, row in df_neighborhoods.iterrows():
-    geojson_data = json.loads(row['geo_shape'])
-    folium.GeoJson(geojson_data).add_to(m)
-
-    # Add a marker in the middle of each neighborhood
-    folium.Marker(
-        location=[row['latitude'], row['longitude']],
-        # Create a popup with the name and other information
-        popup=folium.Popup(f"Name: {row['BUURTNAAM']}, District: {row['WIJKNAAM']}", max_width=250)
-    ).add_to(m)
+                geojson_data = json.loads(row['geo_shape'])
+                folium.GeoJson(geojson_data).add_to(m)
+                folium.Marker(
+                    location=[row['latitude'], row['longitude']],
+                    # Create a popup with the name and other information
+                    popup=folium.Popup(f"Name: {row['BUURTNAAM']}, District: {row['WIJKNAAM']}", max_width=250)
+                ).add_to(m)
+                folium.PolyLine([(row['latitude'], row['longitude']), (row2['latitude'], row2['longitude'])], color="blue", weight=2.5, opacity=1).add_to(m)
 
 # Display the map in Streamlit
 folium_static(m)
 
 
+# Add nodes and edges to the graph
+# for index, row in df_neighborhoods.iterrows():
+#     for index2, row2 in df_neighborhoods.iterrows():
+#         if index != index2:
+#             distance = calculate_distance((row['latitude'], row['longitude']), (row2['latitude'], row2['longitude']))
+#             if distance <= threshold:  # Only add connections under threshold
+#                 G.add_node(row['BUURTNAAM'], pos=(row['latitude'], row['longitude']))
+#                 G.add_node(row2['BUURTNAAM'], pos=(row2['latitude'], row2['longitude']))
+#                 G.add_edge(row['BUURTNAAM'], row2['BUURTNAAM'])
+
+# # Draw the graph
+# nx.draw(G, with_labels=True)
+
+# st.pyplot()
