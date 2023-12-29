ad32d14efaedcdff4261e282411d094556d665cb
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-29 11:50:54 +0100

    added comments

--- pages/15_minute_city_alex.py
+++ pages/15_minute_city_alex.py
@@ -9,30 +9,20 @@ from streamlit_folium import folium_static
 
 # Loading the data
 df_neighborhoods = pd.read_csv("./data/buurten_data_alex.csv", sep=';')
+df_neighborhoods[['latitude', 'longitude']] = df_neighborhoods['geo_point_2d'].str.split(",", expand=True).astype(float)
 
 st.title("15 Minutes City")
-
 st.subheader("Find the closest path from A to B")
 
 # Add an input for the minutes
 minutes = st.number_input("Enter the minutes:", min_value=0, max_value=60, step=1, value=15)
-
-threshold = minutes * 4 / 60
-
-
-st.dataframe(df_neighborhoods)
-
-df_neighborhoods[['latitude', 'longitude']] = df_neighborhoods['geo_point_2d'].str.split(",", expand=True).astype(float)
-
-# Create a graph
-G = nx.Graph()
+threshold = minutes * 4 / 60 # Assuming the average walking speed is 4 km per hour
 
 # Function to calculate distance
 def calculate_distance(point1, point2):
     return geodesic(point1, point2).km
 
-
-# Create a map
+# Create a map using folium
 m = folium.Map(location=[df_neighborhoods['latitude'].mean(), df_neighborhoods['longitude'].mean()], zoom_start=13)
 
 # Add the GeoJSON data to the map and markers only for neighborhoods within the threshold
@@ -54,18 +44,3 @@ for index, row in df_neighborhoods.iterrows():
 # Display the map in Streamlit
 folium_static(m)
 
-
-# Add nodes and edges to the graph
-# for index, row in df_neighborhoods.iterrows():
-#     for index2, row2 in df_neighborhoods.iterrows():
-#         if index != index2:
-#             distance = calculate_distance((row['latitude'], row['longitude']), (row2['latitude'], row2['longitude']))
-#             if distance <= threshold:  # Only add connections under threshold
-#                 G.add_node(row['BUURTNAAM'], pos=(row['latitude'], row['longitude']))
-#                 G.add_node(row2['BUURTNAAM'], pos=(row2['latitude'], row2['longitude']))
-#                 G.add_edge(row['BUURTNAAM'], row2['BUURTNAAM'])
-
-# # Draw the graph
-# nx.draw(G, with_labels=True)
-
-# st.pyplot()
