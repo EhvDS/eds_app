892f53df247c76e7f35563c4d246efa07448490b
Oerlemans,Timo <t.oerlemans@student.fontys.nl>
2024-01-08 11:35:28 +0100

    add color

--- pages/Where_to_go_Timo.py
+++ pages/Where_to_go_Timo.py
@@ -13,17 +13,13 @@ data = pd.read_csv("./data/Timo_Where_to_go.csv", sep=';')
 st.sidebar.header("Map Filters")
 # Add filters here, for example:
 selected_project_phase = st.sidebar.multiselect('Project Phase', data['PROJECTFASE'].unique())
-selected_number_of_homes = st.sidebar.multiselect('Total Number of Homes', data['TOTAALAANTALWONINGEN'].unique())
 selected_name_area = st.sidebar.multiselect('Name Area', data['NAAMDEELGEBIED'].unique())
-selected_number_of_homes_area = st.sidebar.multiselect('Number of homes in area', data['AANTALWONINGENDEELGEBIED'].unique())
 selected_type = st.sidebar.multiselect('Type', data['WONINGTYPE'].unique())
 
 # Filter the data based on selected filters
 filtered_data = data[
     (data['PROJECTFASE'].isin(selected_project_phase)) &
-    (data['TOTAALAANTALWONINGEN'].isin(selected_number_of_homes)) &
     (data['NAAMDEELGEBIED'].isin(selected_name_area)) &
-    (data['AANTALWONINGENDEELGEBIED'].isin(selected_number_of_homes_area))&
     (data['WONINGTYPE'].isin(selected_type))
 ]
 
@@ -33,11 +29,21 @@ st.write(filtered_data)
 # Map creation using Folium
 m = folium.Map(location=[51.45129, 5.45475], zoom_start=12)
 
-# Add markers for filtered data
+# Define a color dictionary based on filters
+color_dict = {
+    'PROJECTFASE_1': 'blue',  # Define colors based on PROJECTFASE values
+    'PROJECTFASE_2': 'green',
+    # Add more colors for different PROJECTFASE values as needed
+}
+
+# Add markers for filtered data with respective colors
 for index, row in filtered_data.iterrows():
+    phase = row['PROJECTFASE']
+    color = color_dict.get(phase, 'red')  # Default color if not found in color_dict
     folium.Marker(
         location=[row['latitude'], row['longitude']],
-        popup=row['naamproject']
+        popup=row['naamproject'],
+        icon=folium.Icon(color=color)
     ).add_to(m)
 
 # Display the map in Streamlit
