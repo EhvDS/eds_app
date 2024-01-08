dea8711e8c34b96ee110408442133dd63630f7f2
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-01-08 13:24:29 +0100

    Update Geoguessr_Kaan_Gogcay.py
    Changed path to .csv

--- pages/Geoguessr_Kaan_Gogcay.py
+++ pages/Geoguessr_Kaan_Gogcay.py
@@ -13,7 +13,7 @@ import pandas as pd
 import random
 
 # Loading images
-df_images = pd.read_csv("../data/kaan_geoguessr_images.csv", index_col=0)
+df_images = pd.read_csv("./data/kaan_geoguessr_images.csv", index_col=0)
 
 # Session state
 if 'score' not in st.session_state:  # number of correct guesses
