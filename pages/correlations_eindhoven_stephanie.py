6a19d738b2ce0da4209e11e0be2821bcff8b49c3
stephaniesmits <90447969+stephaniesmits@users.noreply.github.com>
2024-01-10 16:26:23 +0100

    first version
    First version of data visualization assignment

--- pages/correlations_eindhoven_stephanie.py
+++ pages/correlations_eindhoven_stephanie.py
@@ -1,3 +1,4 @@
+from os import write
 import streamlit as st
 import plotly.express as px
 import pandas as pd
@@ -6,38 +7,80 @@ import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
 import plotly.graph_objects as go
 import numpy as np
+import re
 
 
-df = pd.read_csv('./data/mostrecent_data.csv')
-df_cor = df.drop(columns=['Geoshape', 'GeoshapePerimeter', 'GeoshapeArea', 'GeoshapeCenter', 'NbId', 'DistrictId'])
+df_cor = pd.read_csv('./data/mostrecent_data_stephanie.csv')
 df_cor = df_cor.fillna(0)
-st.write("# Correlations in Eindhoven based on education")
+df_cor = df_cor.replace('.' , 0)
+df_scatter = df_cor
+st.write("# Correlations in Eindhoven: discover yourself!")
 st.write("### Made by Stèphanie Smits")
-
+st.write('##')
+st.write("On this page, multiple data points are collected about the living circumstances in Eindhoven. Play with it yourself and see what interesting correlations you can find!")
+st.write('##')
+#st.table(df_cor.dtypes)
+#st.table(df_scatter.dtypes)
 #st.table(df_cor)
 
+cols = ['number of residents', 'ScoreDiversity', 'NumberHouseholds',
+       'AvgHouseValue', 'PctComplainsAQ', 'PctComplainsNoise',
+       'AvgElectricityUsage', 'PctUnhappy', 'PctHighEducation',
+       'PctLowEducation', 'PctFeelsUnsafe', 'ScoreSocialCohesion',
+       'ScoreGoodLife', 'PctUnemployed', 'PctEconomicallyIndependent',
+       'AvgIncome', 'NumberShops']
+
+
+
+### scatterplot ###
+ 
 y_option = st.selectbox(
  'Select your first attribute',
-  ('PctFeelsUnsafe', 'ScoreGoodLife', 'PctUnhappy', 'ScoreDiversity' 
-   'AvgHouseValue', 'ScoreSocialCohesion', 'PctEconomicallyIndependent', 'PctUnemployed'))
+  (cols))
 
 st.write('You selected:', y_option)
 
 x_option = st.selectbox(
  'Select your second attribute',
-  ('PctLowEducation', 'PctHighEducation'))
+  (cols))
 
 st.write('You selected:', x_option)
 
-fig = px.scatter(data_frame=df_cor, x=x_option, y=y_option, color="number of residents",
-                 color_discrete_sequence=px.colors.qualitative.G10)
+c_option = st.selectbox(
+ 'Select if your want the data based on the living area or the amount of residents per living area',
+  ('NbName', 'number of residents'))
 
+st.write('You selected:', y_option)
+st.write('##')
+
+
+cc = np.corrcoef(df_scatter[y_option].values, df_scatter[x_option].values)[0,1].round(decimals=2)
+
+if y_option == x_option:
+    st.write('##### Try to select two different columns!')
+elif cc < 0.6:
+    st.markdown(f"##### The correlation coefficient is :red[{cc}], which indicates a weak correlation between the {y_option} and the {x_option}")
+elif cc >= 0.75:
+    st.markdown(f"##### The correlation coefficient is :green[{cc}], which indicates a strong correlation between the {y_option} and the {x_option}")
+else:
+    st.markdown(f"##### The correlation coefficient is :blue[{cc}], which indicates a normal correlation between the {y_option} and the {x_option}")
+
+
+
+fig = px.scatter(data_frame=df_scatter, x=x_option, y=y_option, color=c_option,
+                 color_continuous_scale="Reds", trendline="ols", trendline_color_override="white")
 
 st.plotly_chart(fig)
 
-fig2 = px.imshow(
-  df_cor.corr(),
-  color_continuous_scale="Reds",
-)
 
-st.plotly_chart(fig2)
+
+### heatmap ###
+
+# fig2 = px.imshow(
+#   df_cor.corr(),
+#   color_continuous_scale="Reds",
+#   text_auto = True,
+#   aspect = "auto"
+# )
+
+# st.plotly_chart(fig2)
