757ddc298260f19e95dc83be7ce6a7162c7291de
zomerr <5865788+zomerr@users.noreply.github.com>
2024-01-13 10:37:42 +0100

    small changes

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def main():
     st.title("Eindhoven Data Stories")
     st.sidebar.success("Choose ^^^")
 
-    st.markdown("This is a collection of interactive charts, maps and small games based on publicly available data collected in Eindhoven.")
-    st.markdown("Made and maintained at Fontys ICT by students experimenting with data visualization.")
+    st.markdown("This is a collection of interactive charts, maps and small games based on publicly available data about the *Eindhoven city districts and neighbourhoods*.")
+    st.markdown("Made and maintained at [Fontys ICT](https://www.fontysictinnovationlab.nl/) by students experimenting with data visualization.")
     st.markdown("Enjoy!")
 
 if __name__ == '__main__':
