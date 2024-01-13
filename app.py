8734807ea9069c36936718f182a7164beb606833
zomerr <5865788+zomerr@users.noreply.github.com>
2024-01-13 10:50:21 +0100

    last color change for now :-)

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ def main():
         page_title='Eindhoven Data Stories' ,
         page_icon='',   
         layout="wide",
+        theme="dark",
         initial_sidebar_state="expanded")
 
     st.title("Eindhoven Data Stories")
     st.sidebar.success("Choose ^^^")
 
-    st.markdown("This is a collection of interactive charts, maps and small games based on publicly available data about the *Eindhoven city districts and neighbourhoods*.")
+    st.markdown("This is a collection of interactive charts, maps and small games based on publicly available data about the **Eindhoven city districts and neighbourhoods**.")
     st.markdown("Made and maintained at [Fontys ICT](https://www.fontysictinnovationlab.nl/) by students experimenting with data visualization.")
     st.markdown("Enjoy!")
 
