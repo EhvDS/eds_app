545935471ae0f378c82ce761833082847f611507
zomerr <5865788+zomerr@users.noreply.github.com>
2024-01-13 09:57:54 +0100

    small global theme changes

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from PIL import Image
 
 # MAIN
 def main():
-    st.set_page_config(page_title='Eindhoven Data Stories' ,page_icon='')
-    st.title("Eindhoven Data Stories")
-    st.sidebar.success(".")
+    st.set_page_config(
+        page_title='Eindhoven Data Stories' ,
+        page_icon='',   
+        layout="wide",
+        initial_sidebar_state="expanded")
 
+    st.title("Eindhoven Data Stories")
+    st.sidebar.success("Choose ^^^")
+
+    st.markdown("This is a collection of interactive charts, maps and small games based on publicly available data collected in Eindhoven.")
     st.markdown("Made and maintained at Fontys ICT by students experimenting with data visualization.")
+    st.markdown("Enjoy!")
 
 if __name__ == '__main__':
     main()
