13c3d74edfe7d5a02dc91a8dea2c3d652aa81fc7
zomerr <5865788+zomerr@users.noreply.github.com>
2024-01-29 14:05:36 +0100

    added warning that vizzes are not verified

--- EINDHOVEN_IN_CHARTS.py
+++ EINDHOVEN_IN_CHARTS.py
@@ -9,10 +9,18 @@ def main():
         initial_sidebar_state="expanded")
 
     st.title("Eindhoven Data Stories")
-    st.sidebar.success("Choose ^^^")
+    ##st.sidebar.success("Choose ^^^")
 
-    st.markdown("This is a collection of interactive charts, maps and small games based on publicly available data about the **Eindhoven city districts and neighbourhoods**.")
-    st.markdown("Made and maintained at [Fontys ICT](https://www.fontysictinnovationlab.nl/) by students experimenting with data visualization.")
+    st.markdown("This is a collection of interactive charts, maps and small games based on \
+                publicly available data about the **Eindhoven city districts and neighbourhoods**.\
+                Choose on the sidebar which one you want to see.")
+    
+    st.markdown("Made in winter 2023 at [Fontys ICT](https://www.fontysictinnovationlab.nl/) by students experimenting with data visualization.\
+                All data and code is [available](https://github.com/EhvDS/eds_app) but not yet properly reviewed and checked. \
+                So __please don't base any serious conclusions on these visualizations (yet)__.")
+
+    st.markdown("Send your questions and comments to Simona Orzan ([s.orzan@fontys.nl](mailto:s.orzan@fontys.nl)).")
+
     st.markdown("Enjoy!")
 
 if __name__ == '__main__':
